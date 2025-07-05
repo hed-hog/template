72ba40e0c279b5a3c0269d5271bf580af51c3a42
Joao Rangel <joaohcrangel@gmail.com>
2025-07-04 21:38:40 -0300

    Refactor code structure for improved readability and maintainability

--- packages/api-mail/src/mail.consts.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.MAIL_MODULE_OPTIONS = void 0;
-exports.MAIL_MODULE_OPTIONS = 'MAIL_MODULE_OPTIONS';
-//# sourceMappingURL=mail.consts.js.map
--- packages/api-pagination/src/databases/database.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Database = void 0;
-var Database;
-(function (Database) {
-    Database["POSTGRES"] = "postgres";
-    Database["MYSQL"] = "mysql";
-})(Database || (exports.Database = Database = {}));
-//# sourceMappingURL=database.js.map
